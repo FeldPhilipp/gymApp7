70de94b3d76c37894663f0f010eb498802876691
FeldPhilipp <pfeld@rf-computer.de>
2026-06-10 12:09:21 +0200

    upd

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/adminController');
 router.get('/termine', controller.getAllTermine);
 router.post('/addUebung', controller.addNewUebung);
 router.get('/logs', controller.getLogs);
+router.get('/individual', controller.getIndividual);
 
 
 module.exports = router;
